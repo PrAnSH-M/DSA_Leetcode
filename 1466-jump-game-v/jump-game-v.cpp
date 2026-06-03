c42f6a839ee417449eeca1bcb1cfc15d0ddb432e
PrAnSH-M <mauryapransh8@gmail.com>
2026-06-03 13:14:20 +0530

    Time: 14 ms (41.37%) | Memory: 20.7 MB (11.92%) - LeetSync

--- 1466-jump-game-v/jump-game-v.cpp
+++ 1466-jump-game-v/jump-game-v.cpp
@@ -1,33 +1,76 @@
 class Solution {
 public:
-    int n;
-    vector<int> t;
-    int solve(vector<int>& arr, int i, int d) {
-        if(t[i] != -1) return t[i];
-        int result = 1;
+    // int n;
+    // vector<int> t;
+    // int solve(vector<int>& arr, int i, int d) {
+    //     if(t[i] != -1) return t[i];
+    //     int result = 1;
 
-        // left
-        for(int j = i-1; j >= max(0, i-d); j--) {
-            if(arr[j] >= arr[i]) break;
-            result = max(result, 1 + solve(arr, j, d));
-        }
+    //     // left
+    //     for(int j = i-1; j >= max(0, i-d); j--) {
+    //         if(arr[j] >= arr[i]) break;
+    //         result = max(result, 1 + solve(arr, j, d));
+    //     }
 
-        // right
-        for(int j = i+1; j <= min(n-1, i+d); j++) {
-            if(arr[j] >= arr[i]) break;
-            result = max(result, 1 + solve(arr, j, d));
-        }
-        return t[i] = result;
-    }
+    //     // right
+    //     for(int j = i+1; j <= min(n-1, i+d); j++) {
+    //         if(arr[j] >= arr[i]) break;
+    //         result = max(result, 1 + solve(arr, j, d));
+    //     }
+    //     return t[i] = result;
+    // }
+
+    // int maxJumps(vector<int>& arr, int d) {
+    //     n = arr.size();
+    //     int result = 1;
+    //     t.resize(n+1, -1);
+    //     for(int i=0;i<n; i++) {
+    //         result = max(result, solve(arr, i, d));
+    //     }
+
+    //     return result;
+    // }
 
     int maxJumps(vector<int>& arr, int d) {
-        n = arr.size();
-        int result = 1;
+        int n = arr.size();
+        vector<int> t;
         t.resize(n+1, -1);
-        for(int i=0;i<n; i++) {
-            result = max(result, solve(arr, i, d));
+        
+        vector<pair<int, int>> vec;
+
+        for(int i=0;i<n;i++) {
+            vec.push_back({arr[i], i});
         }
 
-        return result;
+        sort(begin(vec), end(vec));
+
+        for(auto& it: vec) {
+            int val = it.first;
+            int i = it.second;
+
+            int result = 1;
+
+            // left
+            for(int j = i-1; j>= max(0, i-d); j--) {
+                if(arr[j] >= arr[i]) break;
+                result = max(result, 1 + t[j]);
+            }
+
+            // right
+            for(int j=i+1; j<= min(n-1, i+d); j++) {
+                if(arr[j] >= arr[i]) break;
+
+                result = max(result, 1 + t[j]);
+            }
+
+            t[i] = result;
+        }
+
+        int maxCount = 1;
+
+        for(int i=0;i<n;i++){
+            maxCount = max(maxCount, t[i]);
+        }
+        return maxCount;
     }
 };
