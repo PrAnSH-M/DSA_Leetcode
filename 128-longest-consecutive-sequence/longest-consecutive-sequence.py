af0701228bdffcf05ae227d6a7089b0f069c8af3
PrAnSH-M <mauryapransh8@gmail.com>
2026-08-17 13:04:29 +0530

    Time: 47 ms (70.64%) | Memory: 36.5 MB (66.45%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -4,19 +4,19 @@ class Solution:
         # print(hashset)
 
         max_len = 0
-        current_num = 0
-        current_len = 0
+        # current_num = 0
+        # current_len = 0
 
         for num in hashset:
             if num - 1 not in hashset:
                 current_num = num
                 current_len = 1
             
-            while current_num + 1 in hashset:
-                current_len += 1
-                current_num += 1
-            
-            max_len = max(max_len, current_len)
+                while current_num + 1 in hashset:
+                    current_len += 1
+                    current_num += 1
+                
+                max_len = max(max_len, current_len)
 
         return max_len
 
